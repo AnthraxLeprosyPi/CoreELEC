c7edd13438cf9202dc4d4d6dd59fc3ab5ecf9712
AnthraxLeprosyPi <anthrax.leprosy.pi@gmail.com>
2018-11-06 14:48:48 +0100

    Changes to use the new Hue Entertainment API in Hyperion.NG
    Signed-off-by: AnthraxLeprosyPi <anthrax.leprosy.pi@gmail.com>

--- packages/addons/service/hyperion.ng/package.mk
+++ packages/addons/service/hyperion.ng/package.mk
@@ -6,8 +6,8 @@ PKG_VERSION="e438bc62a651c1e6a13396e1430f5687a6ef703f"
 PKG_SHA256="1bac2401e3c0ee7ff9efa266d87f57512c43b4b3507e563e32c3060a2f2d8fd1"
 PKG_REV="100"
 PKG_LICENSE="GPL"
-PKG_SITE="https://github.com/hyperion-project/hyperion.ng"
-PKG_URL="https://github.com/hyperion-project/hyperion.ng/archive/$PKG_VERSION.tar.gz"
+PKG_SITE="https://github.com/AnthraxLeprosyPi/hyperion.ng"
+PKG_URL="https://github.com/AnthraxLeprosyPi/hyperion.ng/archive/$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain Python3 avahi libusb qt-everywhere protobuf"
 PKG_SECTION="service"
 PKG_SHORTDESC="Hyperion.NG: an AmbiLight controller"
@@ -54,6 +54,7 @@ PKG_CMAKE_OPTS_TARGET="-DCMAKE_NO_SYSTEM_FROM_IMPORTED=ON \
                        $PKG_FB_SUPPORT \
                        $PKG_DENABLE_WS281XPWM \
                        $PKG_X11_SUPPORT \
+					   -DENABLE_HUEENTERTAINMENT=ON\
                        -DENABLE_OSX=0 \
                        -DENABLE_SPIDEV=1 \
                        -DENABLE_TINKERFORGE=0 \
